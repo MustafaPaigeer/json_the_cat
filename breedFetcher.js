85eba4c0271ca68279b69815e3b2362d35d93df4
Mustafa Paigeer <paigeer2012@gmail.com>
2022-02-18 22:24:43 +0000

    refactored breedFetcher.js added index.js and mochaTest

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,26 +1,21 @@
 const request = require('request');
-const breedName = process.argv.slice(2);
 
-const options = {
-  url: `https://api.thecatapi.com/v1/breeds/search?name=${breedName}`,
-  headers: {
-    'User-Agent': 'request'
-  }
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?name=${breedName}`, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      if (body === '[]') {
+        callback(null, `Breed name ${breedName} not found`);
+      } else {
+        const data = JSON.parse(body);
+        callback(null, data[0].description);
+      }
+    } else {
+      callback(error, null);
+    }
+  });
 };
-  
-request(options, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    if (body === '[]') {
-      console.log(`Breed name ${breedName} not found`);
-    }
-    const data = JSON.parse(body);
-    for (const item of data) {
-      console.log(item.description);
-    }
-  } else {
-    console.log(`error: ${error}`);
-  }
-});
+
+module.exports = { fetchBreedDescription };
 
 
 
